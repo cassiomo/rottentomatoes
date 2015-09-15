b9c1bbedeffed1694a3a6732df58502916b67c01
Cassio Mo <cassiomo@gmail.com>
2015-09-14 22:03:53 -0700

    first commit

---
+++ RottenTomatoes/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  RottenTomatoes
+//
+//  Created by Mo, Kevin on 7/26/15.
+//  Copyright (c) 2015 Mo, Kevin. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
