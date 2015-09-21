c9536b71e317285acee1b761eb84b2b402732ab9
Cassio Mo <cassiomo@gmail.com>
2015-09-20 21:52:16 -0700

    attempt to add searchbar

--- RottenTomatoes/TabSwitchViewController.swift
+++ RottenTomatoes/TabSwitchViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class TabSwitchViewController : UIViewController {
     
+    
+    var searchBar : UISearchBar!
+    
+//    override func viewDidAppear(animated: Bool) {
+//        super.viewDidAppear(animated)
+//        
+//        searchBar = UISearchBar(frame: CGRectMake(0.0, -80.0, 320.0, 44.0))
+//        navigationController?.navigationBar.addSubview(searchBar)
+//        
+//        var button = UIBarButtonItem(image: <#T##UIImage?#>, style: <#T##UIBarButtonItemStyle#>, target: <#T##AnyObject?#>, action: <#T##Selector#>)
+//
+//        self.navigationItem.rightBarButtonItem = button
+//    }
+//
+//    override func showViewController(vc: UIViewController, sender: AnyObject?) {
+//        //searchBar.frame = CGRectMake(0.0, 0, 320, 44)
+//    }
+//    
+//    override func viewWillDisappear(animated: Bool) {
+//        //searchBar.removeFromSuperview()
+//    }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let viewController = segue.destinationViewController as! ViewController
         switch restorationIdentifier! {
